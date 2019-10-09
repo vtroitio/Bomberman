aae674ceb3cbb7e088a417ad605542f5bb62f057
Elkronos <diegortabarez@gmail.com>
2019-10-09 19:26:29 -0300

    Cambio background y GameEngine

--- src/background.py
+++ src/background.py
@@ -10,24 +10,34 @@ class Background():
         self.screen = pygame.display.set_mode(dimensions)
         self.dimensions = dimensions
         self.bomberman = None
-        self.game = game
-        
-        # self.game = game.Game()
+        self.game = game        # self.game = game.Game()
+
+# Reload
+    def reloadBackground(self, resolucion):
+        for i in range(0, int((resolucion[0] / 37)) + 1):    # Creo Las Filas
+            self.screen.blit(self.obstacle, (i * 37, 0))
+            self.screen.blit(self.obstacle, (i * 37, resolucion[1] - 37))
+
+        for i in range(0, int((resolucion[1] / 37)) + 1):   # Creo las columnas
+            self.screen.blit(self.obstacle, (0, i * 37))
+            self.screen.blit(self.obstacle, (resolucion[0] - 37, i * 37))
+
+    def reloadBomberman(self):
+        self.screen.blit(self.bomberman, self.game.getBombermanPosition())
+
+# Loads
+    def loadObstacle(self, path, pos):
+        self.obstacle = pygame.image.load(path)
+        self.screen.blit(self.obstacle, pos)
+
+    def loadBackgroundImage(self, path):
+        self.background = pygame.image.load(path)
 
     def loadBombermanImage(self, path, pos):
         self.bomberman = pygame.image.load(path)
         self.screen.blit(self.bomberman, pos)
 
-    def reloadBackground(self):
-        self.screen.blit(self.background, [0, 0])
-
-    def loadBackgroundImage(self, path):
-        self.background = pygame.image.load(path)
-        self.screen.blit(self.background, (0, 0))
-
-    def reloadBomberman(self):
-        self.screen.blit(self.bomberman, self.game.getBombermanPosition())
-    
+# Etc
     def fillBlack(self):
         color = (0, 0, 0)
         self.screen.fill(color)
--- src/gameEngine.py
+++ src/gameEngine.py
@@ -8,9 +8,10 @@ CONTROLES = {'273': [0, -1], '274': [0, 1], '275': [1, 0], '276': [-1, 0]}
 class GameEngine():
     def __init__(self):
         self.game = game.Game()
-        self.dimensions = (555, 407)
+        self.dimensions = [962, 592]
         self.background = background.Background(self.dimensions, self.game)
         self.loadImages()
+        self.background.reloadBackground(self.dimensions)
         self.mainLoop()
 
     def esc():
@@ -29,8 +30,9 @@ class GameEngine():
         pass
 
     def loadImages(self):
-        self.background.loadBackgroundImage("sprites/muro.png")
+        self.background.loadBackgroundImage("sprites/pilar.png")
         self.background.loadBombermanImage('sprites/Bomberman.png', (37, 37))
+        self.background.loadObstacle("sprites/pilar.png", (59, 59))
 
     def mainLoop(self):
         while True:
@@ -38,9 +40,12 @@ class GameEngine():
                 if event.type == pygame.QUIT: sys.exit()
                 if event.type == pygame.KEYDOWN:
                     self.background.fillBlack()
+
                     self.game.givePosition(CONTROLES[str(event.key)])
+                    self.background.loadObstacle("sprites/pilar.png", (59, 59))
                     self.background.reloadBomberman()
-                    self.background.reloadBackground()
+
+                    self.background.reloadBackground(self.dimensions)
                 pygame.display.flip()
 
 if __name__ == "__main__":
